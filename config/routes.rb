98baa0671ac3aac0b2b431df49fc7bc030d84156
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-01-08 13:08:59 +0100

    Adds PerformanceData controller, routes and spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
 
   namespace :api do
-    namespace :v1 do
+    namespace :v1, defaults: { formats: :json } do
+      resources :performance_data, only: [:create]
     end
   end
 end

662d610746da9a82369fff5e6f12aa569bec90b1
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-01-08 10:48:37 +0100

    Adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
+
+  namespace :api do
+    namespace :v1 do
+    end
+  end
 end
